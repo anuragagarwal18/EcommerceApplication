d63143702ca2411f5c173457cb023ec86df986c8
anuragagarwal18 <anuragagarwal1804@gmail.com>
2018-05-07 19:34:33 +0530

    delete

--- models.js
+++
@@ -1,7 +0,0 @@
-exports = module.exports = function (app, mongoose) {
-
-	require('./schemas/product')(app, mongoose);
-	require('./schemas/category')(app, mongoose);
-	require('./schemas/cart')(app, mongoose);
-
-};
--- routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (app) {
-    app.get('/getItems', require('./rest_api/index').getItems);
-    app.put('/addItemsToCart/', require('./rest_api/index').addItemsToCart);
-    app.get('/getCartDetails/:userId', require('./rest_api/index').getCartDetails);
-    app.put('/removeItemFromCart/', require('./rest_api/index').removeItemFromCart);
-};
